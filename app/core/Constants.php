a64a9d4d5873d71b0fb3b66d6b8352ab77dd226d
devcopas <borjutimothy@gmail.com>
2021-03-18 18:50:45 +0700

    ASSETS, add bootstrap

---
+++ app/core/Constants.php
@@ -0,0 +1,8 @@
+<?php
+
+// define('BASEURL','http://localhost/php-mvc/public');
+
+
+$root = ( isset($_SERVER['HTTPS']) ? "https://" : "http://" ) . $_SERVER['HTTP_HOST'];
+// $root .= str_replace( basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+define('BASEURL', $root);
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,7 @@
-<h1>About Me</h1>
-<p>Hello, My name is <?= $data['name']; ?>, I'm <?= $data['age']; ?> years old <?= $data['job']; ?></p>
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
+	<img class="img-fluid" src="<?= BASEURL ?>/img/1.jpg">
+	<p class="fw-bold text-center mt-2">
+	Hello, My name is <?= $data['name']; ?>, I'm <?= $data['age']; ?> years old <?= $data['job']; ?>
+	</p>
+</div>
